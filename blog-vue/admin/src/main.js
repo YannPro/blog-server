3a54befda65eea24aea359e3434817ed46b08790
YannYao <849723885@qq.com>
2018-01-22 13:39:50 +0800

    【修改】admin模版

--- blog-vue/admin/src/main.js
+++ blog-vue/admin/src/main.js
@@ -1,44 +1,106 @@
-import Vue from 'vue';
-import iView from 'iview';
-import {router} from './router/index';
-import {appRouter} from './router/router';
-import store from './store';
-import App from './app.vue';
-import '@/locale';
-import 'iview/dist/styles/iview.css';
-import VueI18n from 'vue-i18n';
-import util from './libs/util';
+import babelpolyfill from 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+import NProgress from 'nprogress'//页面顶部进度条
+import 'nprogress/nprogress.css'
 
-Vue.use(VueI18n);
-Vue.use(iView);
+import Login from './pages/Login.vue'
+import Home from './pages/Home.vue'
+import Main from './pages/Main.vue'
+import Table from './pages/nav1/Table.vue'
+import Form from './pages/nav1/Form.vue'
+import user from './pages/nav1/user.vue'
+import Page4 from './pages/nav2/Page4.vue'
+import Page5 from './pages/nav2/Page5.vue'
+import Page6 from './pages/nav3/Page6.vue'
+import echarts from './pages/charts/echarts.vue'
+
+// start mock
+import Mock from './mock';
+Mock.bootstrap();
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+NProgress.configure({ showSpinner: false });
+
+const routes = [
+  {
+    path: '/login',
+    component: Login,
+    hidden: true//不显示在导航中
+  },
+  //{ path: '/main', component: Main },
+  {
+    path: '/',
+    component: Home,
+    name: '导航一',
+    iconCls: 'el-icon-message',//图标样式class
+    children: [
+      //{ path: '/main', component: Main },
+      { path: '/table', component: Table, name: 'Table' },
+      { path: '/form', component: Form, name: 'Form' },
+      { path: '/user', component: user, name: '列表' },
+    ]
+  },
+  {
+    path: '/',
+    component: Home,
+    name: '导航二',
+    iconCls: 'fa fa-id-card-o',
+    children: [
+      { path: '/page4', component: Page4, name: '页面4' },
+      { path: '/page5', component: Page5, name: '页面5' }
+    ]
+  },
+  {
+    path: '/',
+    component: Home,
+    name: '',
+    iconCls: 'fa fa-address-card',
+    leaf: true,//只有一个节点
+    children: [
+      { path: '/page6', component: Page6, name: '导航三' }
+    ]
+  },
+  {
+    path: '/',
+    component: Home,
+    name: 'Charts',
+    iconCls: 'fa fa-bar-chart',
+    children: [
+      { path: '/echarts', component: echarts, name: 'echarts' }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next()
+})
+
+router.afterEach(transition => {
+  NProgress.done();
+});
 
 new Vue({
-    el: '#app',
-    router: router,
-    store: store,
-    render: h => h(App),
-    data: {
-        currentPageName: ''
-    },
-    mounted () {
-        this.currentPageName = this.$route.name;
-        // 显示打开的页面的列表
-        this.$store.commit('setOpenedList');
-        this.$store.commit('initCachepage');
-        // 权限菜单过滤相关
-        this.$store.commit('updateMenulist');
-        // iview-admin检查更新
-        util.checkUpdate(this);
-    },
-    created () {
-        let tagsList = [];
-        appRouter.map((item) => {
-            if (item.children.length <= 1) {
-                tagsList.push(item.children[0]);
-            } else {
-                tagsList.push(...item.children);
-            }
-        });
-        this.$store.commit('setTagsList', tagsList);
-    }
-});
+  el: '#app',
+  template: '<App/>',
+  router,
+  store,
+  components: { App }
+  //render: h => h(Login)
+}).$mount('#app')
+
+//router.replace('/login')
+
